5b1dd2773a573b81b1139218f7416dd06baa76e5
nihadafza <nihad.afza123@gmail.com>
2020-12-22 16:36:19 +0530

    Numpy_Operations

---
+++ nihad/Python-for-Data-Analysis-Numpy/Numpy_Operations.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+#Arithmetic Functions
+arr = np.arange(0,10)
+
+print(arr + arr)
+
+print(arr * arr)
+
+print(arr - arr)
+
+print(arr/arr)
+
+print(1/arr)
+
+print(arr**3)
+
+#Universal Array Functions
+#Taking Square Roots
+print(np.sqrt(arr))
+
+#Calcualting exponential (e^)
+print(np.exp(arr))
+
+print(np.max(arr))
+
+print(np.sin(arr))
+
+print(np.log(arr))
