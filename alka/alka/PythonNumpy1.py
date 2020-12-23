aee94eaef4da0740440bebd3edb66b8e6bd076bb
alkasinha1234 <alkasinha2014@gmail.com>
2020-12-23 12:22:41 +0530

    Numpy1

---
+++ alka/alka/PythonNumpy1.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+my_list = [1,2,3]
+print(my_list)
+
+my_matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(my_matrix)
+
+print(np.array(my_matrix))
+
+print(np.arange(0,10))
+
+print(np.arange(0,11,2))
+
+print(np.zeros(3))
+
+print(np.zeros((5,5)))
+
+print(np.ones(3))
+
+print(np.ones((3,3)))
+
+print(np.linspace(0,10,3))
+
+print(np.linspace(0,10,50))
+
+print(np.eye(4))
+
+print(np.random.rand(2))
+
+print(np.random.rand(5,5))
+
+print(np.random.randint(1,100))
+
+print(np.random.randint(1,100,10))
+
